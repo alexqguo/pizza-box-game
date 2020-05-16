e55346bc3f225b328d491ab31247f548f7ac2fe1
Alberto Navarro <navio@users.noreply.github.com>
2020-05-16 18:27:17 -0400

    Spanish corrections and enhacement for other languages (#2)
    * Spanish corrections and enhacement for other languages
    
    * removing tranlsations

--- src/components/Translation.tsx
+++ src/components/Translation.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import en_US from '../i18n/en_US.json';
-const stringTranslation = en_US;
+import React, {useState, useEffect} from 'react';
+import en from '../i18n/en_US.json';
+import es from '../i18n/es_MX.json';
 
-// import {LanguageContext} from './Translation'
-// const i18n = useContext(LanguageContext);
+const languages: {[key: string]: any} = {  en, es };
+const browserLanguage: string =  navigator?.language.split('-')[0] || 'en';
 
-export const LanguageContext = React.createContext<any>('us_EN');
+export const LanguageContext = React.createContext<any>(en);
 export const LanguageProvider = ({ children }: any) => {
+  const [language, setLanguage] = useState(en);
+  useEffect(()=> {
+    const language = languages[browserLanguage];
+    if(language){
+      setLanguage(language)
+    }
+  },[])
+  
+  
   return (
-    <LanguageContext.Provider value={stringTranslation}>
+    <LanguageContext.Provider value={language}>
       {children}
     </LanguageContext.Provider>
   )
